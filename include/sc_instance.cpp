13bc6e9f0e4f06f388c07b48a83b441b35157bb7
Schmoozerd <schmoozerd@scriptdev2.com>
2013-02-15 00:36:33 +0200

    [c2770] Support library doors in Dire Maul
    * Also fix range pylon sorting due to biiig Pylons
    * Also change some Do..GO instance storage functions to only display debug log if the GO was not yet stored
    
    Note: The door handling is sort of vague, but should essentially be correct, especially unclear if the northern door to the library requires similar handling
    
    (based on commit 8a05760)

--- include/sc_instance.cpp
+++ include/sc_instance.cpp
@@ -156,7 +156,7 @@ GameObject* ScriptedInstance::GetSingleGameObjectFromStorage(uint32 uiEntry)
         return instance->GetGameObject(find->second);
 
     // Output log, possible reason is not added GO to map, or not yet loaded;
-    debug_log("SD2: Script requested gameobject with entry %u, but no gameobject of this entry was created yet, or it was not stored by script for map %u.", uiEntry, instance->GetId());
+    script_error_log("Script requested gameobject with entry %u, but no gameobject of this entry was created yet, or it was not stored by script for map %u.", uiEntry, instance->GetId());
 
     return NULL;
 }
@@ -170,7 +170,7 @@ Creature* ScriptedInstance::GetSingleCreatureFromStorage(uint32 uiEntry, bool bS
 
     // Output log, possible reason is not added GO to map, or not yet loaded;
     if (!bSkipDebugLog)
-        debug_log("SD2: Script requested creature with entry %u, but no npc of this entry was created yet, or it was not stored by script for map %u.", uiEntry, instance->GetId());
+        script_error_log("Script requested creature with entry %u, but no npc of this entry was created yet, or it was not stored by script for map %u.", uiEntry, instance->GetId());
 
     return NULL;
 }
