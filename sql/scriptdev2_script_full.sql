740aebf812e1602ef13ee48de50aff1a757aac5d
Xfurry <xfurry@scriptdev2.com>
2013-04-30 17:55:39 +0200

    [c2820] Use the new ScriptEffect call in SD2. Requires CMangos [c12572]
    (based on commit [2820] - 511fa72)
    
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for C-MaNGOS c12470+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS c12572+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
