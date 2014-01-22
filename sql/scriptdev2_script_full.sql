f8826cbedafa5f6c479686f6b027b2450b46c583
Cyberium <cyberium@techemail.com>
2014-01-22 23:47:51 +0100

    [c2983] Fix compile for CMaNGOS c12795
    Thanks to dbauz for pointing. Requires CMaNGOS [12587]
    
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>
    
    (based on commit SD2-WotLK [2983] - 69456a5)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS c12752+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS c12795+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
