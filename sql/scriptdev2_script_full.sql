2463afecc858bf1127102775dd983befbf385e84
Xfurry <xfurry@scriptdev2.com>
2014-09-02 21:45:24 +0200

    [c3018] Implement NpcSpellClick script hook. Requires CMaNGOS c12872+
    (based on commit [3018] - 1ff9aea)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS c12867+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS c12872+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
