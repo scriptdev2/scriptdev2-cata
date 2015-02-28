a088ae160f5d193199d0af447a3717c41832b36f
Xfurry <xfurry@scriptdev2.com>
2015-02-28 23:36:00 +0100

    [c3077] Make use of the new OnCreatureDespawn feature in ToC5
    This allows vehicle mounts to respawn on passenger eject
    
    Requires Cmangos c13014+
    
    (based on commit SD2[3077] - f1d8362)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS c13010+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS c13014+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
